39d9a3f3f325e0ed5e24c5273c630c2881a56d6c
Hubert Gorzkowski <h-gorzkowski1@wp.pl>
2023-05-05 00:39:34 +0200

    graphics optimization, add slider,

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//navigation animation
 const menu = document.querySelector("nav");
 
 const menuAnimation = function () {
@@ -6,7 +7,42 @@ const menuAnimation = function () {
   } else if (window.scrollY < 60) {
     menu.classList.remove("smallerMenu");
   }
-  console.log(window.scrollY);
 };
 
 window.addEventListener("scroll", menuAnimation);
+
+//slider in div bestProject
+const bestProjectImages = [
+  "picture/aimTraining1.PNG",
+  "picture/aimTraining2.PNG",
+  "picture/aimTraining3.PNG",
+  "picture/aimTraining4.PNG",
+];
+
+let numberOfPhoto = 1;
+let infoAboutPhoto = document.querySelector("p.numberOfPhotos");
+const image = document.querySelector(".imgOfProject--best");
+
+const slider = function () {
+  if (this.classList.value == "fas fa-arrow-left arrowSlider") {
+    if (numberOfPhoto == 1) {
+      numberOfPhoto = 4;
+    } else {
+      numberOfPhoto--;
+    }
+    image.src = `picture/aimTraining${numberOfPhoto}.PNG`;
+  } else if (this.classList.value == "fas fa-arrow-right arrowSlider") {
+    if (numberOfPhoto == 4) {
+      numberOfPhoto = 1;
+    } else {
+      numberOfPhoto++;
+    }
+    image.src = `picture/aimTraining${numberOfPhoto}.PNG`;
+  }
+  infoAboutPhoto.innerHTML = `${numberOfPhoto}/4`;
+};
+
+const arrows = document.querySelectorAll("i.arrowSlider");
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", slider);
+});
