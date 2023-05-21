f906099c84ec46aab659ded9c10a89f2715b5d28
Hubert Gorzkowski <h-gorzkowski1@wp.pl>
2023-05-21 15:58:42 +0200

    add scroller to others projects

--- script.js
+++ script.js
@@ -2,47 +2,110 @@
 const menu = document.querySelector("nav");
 
 const menuAnimation = function () {
-  if (window.scrollY > 60) {
-    menu.classList.add("smallerMenu");
-  } else if (window.scrollY < 60) {
-    menu.classList.remove("smallerMenu");
-  }
+	if (window.scrollY > 60) {
+		menu.classList.add("smallerMenu");
+	} else if (window.scrollY < 60) {
+		menu.classList.remove("smallerMenu");
+	}
 };
 
 window.addEventListener("scroll", menuAnimation);
 
-//slider in div bestProject
-const bestProjectImages = [
-  "..//picture/aimTraining1.PNG",
-  "..//picture/aimTraining2.PNG",
-  "..//picture/aimTraining3.PNG",
-  "..//picture/aimTraining4.PNG",
-];
+//sliders
 
-let numberOfPhoto = 1;
-let infoAboutPhoto = document.querySelector("p.numberOfPhotos");
-const image = document.querySelector(".imgOfProject--best");
+// const stopwatchImages = [
+// 	"..//picture/stopwatch1.PNG",
+// 	"..//picture/stopwatch2.PNG",
+// ];
 
-const slider = function () {
-  if (this.classList.value == "fas fa-arrow-left arrowSlider") {
-    if (numberOfPhoto == 1) {
-      numberOfPhoto = 4;
-    } else {
-      numberOfPhoto--;
-    }
-    image.src = `..//picture/aimTraining${numberOfPhoto}.PNG`;
-  } else if (this.classList.value == "fas fa-arrow-right arrowSlider") {
-    if (numberOfPhoto == 4) {
-      numberOfPhoto = 1;
-    } else {
-      numberOfPhoto++;
-    }
-    image.src = `..//picture/aimTraining${numberOfPhoto}.PNG`;
-  }
-  infoAboutPhoto.innerHTML = `${numberOfPhoto}/4`;
+let numberOfPhoto1 = 1;
+let numberOfPhoto2 = 1;
+let numberOfPhoto3 = 1;
+
+const slider = function (arrow, images, photoCounter, picture, whichCounter) {
+	if (whichCounter == 1) {
+		if (arrow.dataset.direction == "left") {
+			if (numberOfPhoto1 == 1) {
+				numberOfPhoto1 = images.length;
+			} else {
+				numberOfPhoto1--;
+			}
+		} else if (arrow.dataset.direction == "right") {
+			if (numberOfPhoto1 == images.length) {
+				numberOfPhoto1 = 1;
+			} else {
+				numberOfPhoto1++;
+			}
+		}
+		picture.src = images[numberOfPhoto1 - 1];
+		photoCounter.innerHTML = `${numberOfPhoto1}/${images.length}`;
+	} else if (whichCounter == 2) {
+		if (arrow.dataset.direction == "left") {
+			if (numberOfPhoto2 == 1) {
+				numberOfPhoto2 = images.length;
+			} else {
+				numberOfPhoto2--;
+			}
+		} else if (arrow.dataset.direction == "right") {
+			if (numberOfPhoto2 == images.length) {
+				numberOfPhoto2 = 1;
+			} else {
+				numberOfPhoto2++;
+			}
+		}
+		console.log(images);
+		picture.src = images[numberOfPhoto2 - 1];
+		photoCounter.innerHTML = `${numberOfPhoto2}/${images.length}`;
+	} else if (whichCounter == 3) {
+		if (arrow.dataset.direction == "left") {
+			if (numberOfPhoto3 == 1) {
+				numberOfPhoto3 = images.length;
+			} else {
+				numberOfPhoto3--;
+			}
+		} else if (arrow.dataset.direction == "right") {
+			if (numberOfPhoto3 == images.length) {
+				numberOfPhoto3 = 1;
+			} else {
+				numberOfPhoto3++;
+			}
+		}
+		picture.src = images[numberOfPhoto3 - 1];
+		photoCounter.innerHTML = `${numberOfPhoto3}/${images.length}`;
+	}
+};
+
+const whichSlider = function () {
+	if (this.dataset.whickslider == "bestSlider") {
+		const images = [
+			"..//picture/aimTraining1.PNG",
+			"..//picture/aimTraining2.PNG",
+			"..//picture/aimTraining3.PNG",
+			"..//picture/aimTraining4.PNG",
+		];
+		let photoCounter = document.querySelector(
+			"div.bestProject p.numberOfPhotos"
+		);
+		let whichCounter = 1;
+		const picture = document.querySelector(".imgOfProject--best");
+
+		slider(this, images, photoCounter, picture, whichCounter);
+	} else if (this.dataset.whickslider == "memorySlider") {
+		const images = [
+			"..//picture/memoryGame1.PNG",
+			"..//picture/memoryGame3.PNG",
+			"..//picture/memoryGame2.PNG",
+		];
+		let photoCounter = document.querySelector(
+			"div.slider p.numberOfPhotosOther1"
+		);
+		let whichCounter = 2;
+		const picture = document.querySelector(".imgOfProject1");
+		slider(this, images, photoCounter, picture, whichCounter);
+	}
 };
 
 const arrows = document.querySelectorAll("i.arrowSlider");
 arrows.forEach((arrow) => {
-  arrow.addEventListener("click", slider);
+	arrow.addEventListener("click", whichSlider);
 });
